1831301ba93152b6f29e21b021b98ff0357d94bf
Aaron Haun <aaron@haun.guru>
2024-01-24 20:32:33 -0500

    feat: docs and config

---
+++ README.md
@@ -0,0 +1,70 @@
+# Klipper Quick Flash
+
+## Project Goals
+* single file (either .py or .pyz)
+* use only python builtin libraries, or those already required by klipper
+* should be usable by folks with only cursory linux knowledge
+
+
+## KQL Configuration
+KQF's configration lives in ~/.kqf/kqf.cfg, and will be generated on first start if it does not exist.
+You can edit you KQF config in your default editor with `kqf editconfig`.
+
+The file is in python config format, which is the same as klipper uses,
+note that KQF does not support the klipper-specific `[include file.cfg]` syntax
+
+### KQF Configration
+KQF's own configuration lives in the 'KQF' section
+
+
+Example:
+```
+[KQF]
+klipper_repo_path: autodetect
+klipper_config_path: autodetect
+config_flavors_path: ~/.kqf/flavors
+firmware_storage: ~/.kqf/firmware
+```
+
+| Option                | Required | Type   | Description                                                                                                                             | Default         |
+|-----------------------|----------|--------|-----------------------------------------------------------------------------------------------------------------------------------------|-----------------|
+| klipper_repo_path     | Yes      | String | Where to find the klipper source, setting this to `autodetect` will cause KQF to try to locate klipper in common locations              | `autodetect`    |
+| klipper_config_path   | No       | String | Where to find the klipper printer.cfg, `autodetect` behaves similar to the above. If empty/missing, config will not be used to find MCU | `autodetect`    |
+| config_flavors_path   | Yes      | String | Where to store configuration flavors                                                                                                    | ~/.kqf/flavors  |
+| firmware_storage_path | Yes      | String | Where to store compiled firmware                                                                                                        | ~/.kqf/firmware |
+
+### Configuring the primary MCU
+The configuration for your `primary` MCU lives in the `mcu` section.
+
+Minimal Example:
+```
+[mcu]
+flavor: octopus_pro_v1_2
+# mcu_type and mcu_chip will be automatically read from the flavor
+# communication_type and communication_id will be extracted from printer.cnf
+# bootloader and flash_method will be guessed from mcu_, communication_, and bootloader settings
+```
+
+Maximal Example:
+```
+[mcu]
+flavor: spelling_everyting_out
+mcu_type: stm32
+mcu_chip: stm32f446xx
+communication_type: usb
+communication_id: a1b2c3d4e5f6
+bootloader: katapult
+flash_method: katapult
+```
+
+### Configuring Additional MCUs.
+Additional MCUs are configured with their own sections, named `[mcu NAME_HERE]` (similar to the way they are in klipper)
+
+The names should match those in your printer.cnf
+
+Example:
+```
+[mcu ebb32]
+flavor: ebb32_v1
+bootloader: katapult
+```
--- kqf.py
+++ kqf.py
@@ -3,7 +3,7 @@ import configparser
 import logging
 import pathlib
 import textwrap
-from typing import Optional, List
+from typing import Optional, List, Union
 from pprint import pprint as pp
 from os import PathLike
 
@@ -17,18 +17,36 @@ def process() -> None:
     The main entrypoint for kqf.
     :return: None
     """
-    logging.getLogger().setLevel(logging.INFO)
-    for i in KlipperMCU.get_from_printer_cfg('test/test.printer.cfg'):
-        print(f"Found {i.connection_type} mcu with id {i.connection_id}")
-    pp(KQFConfig.get())
+    logging.getLogger().setLevel(logging.DEBUG)
+    kqf_conf = KQFConfig.get(path="test/kqf.cfg")
+    if kqf_conf.klipper_config:
+        logging.debug("Loading MCU definitions from klipper configs")
+        klipper_conf = KlipperConf(kqf_conf.klipper_config)
+        logging.debug(
+                        f"Loaded {len(klipper_conf.mcu_names())} MCUs definitions from Klipper: "
+                        f"[{', '.join(klipper_conf.mcu_names())}]")
+
+
+class KlipperConf(object):
+    def __init__(self, filename):
+        self.data = None
+        self.data = configparser.ConfigParser()
+        self.data.read(filename)
+
+        self.__mcu_sections = {(x if x == 'mcu' else x[4:]): self.data[x] for x in self.data.sections() if
+                               x == 'mcu' or x.startswith('mcu ')}
+
+    def mcu_names(self):
+        return self.__mcu_sections.keys()
 
 
 class KlipperMCU(object):
     """
     A representation of an individual MCU
     """
+
     @staticmethod
-    def get_from_printer_cfg(filename: str):
+    def get_from_printer_cfg(filename: PathLike):
         config = configparser.ConfigParser()
         # TODO: handle the import section
         config.read(filename)
@@ -43,7 +61,7 @@ class KlipperMCU(object):
             mcu = KlipperMCU(section_name[4:])
         else:
             raise ValueError(f"mcu cfg section with invalid name {section_name}")
-        mcu.__from_klipper_config(cfg_section)
+        mcu.from_klipper_config(cfg_section)
         return mcu
 
     def __init__(self, name):
@@ -59,7 +77,7 @@ class KlipperMCU(object):
         self.flash_method: str  # The method that will be used to flash this mcu
         self.flavor: str  # The name of the config 'flavor' used, this is the name of the
 
-    def __from_klipper_config(self, config_block):
+    def from_klipper_config(self, config_block):
         if 'serial' in config_block:
             self.connection_type = 'serial'
             self.connection_id = config_block['serial']
@@ -74,7 +92,7 @@ class KlipperMCU(object):
 class KQFMCUConfig(object):
     """
     This represents KQF configuration for a specific MCU it affects how KQF gathers info about the MCU
-    For most mcus, this should be mosty Nones
+    For most MCUs, this should be mostly Nones
     """
     config_flavor: str
     mcu_type: Optional[str]  # Overrides detection from KConfig flavor
@@ -85,16 +103,42 @@ class KQFMCUConfig(object):
     flash_method: Optional[str]  # Overrides value guessed from mcu_type, communication_*, and bootloader
     pass
 
+    @classmethod
+    def from_config(cls, config_section):
+        obj = KQFMCUConfig()
+        obj.config_flavor = config_section.get('flavor')
+        if not obj.config_flavor:
+            raise ValueError("MCU firmware flavor not specified, try making one with 'kqf menuconfig'")
+        obj.mcu_type = config_section.get('mcu_type')
+        obj.mcu_chip = config_section.get('mcu_chip')
+        obj.communication_type = config_section.get('communication_type')
+        obj.communication_id = config_section.get('communication_id')
+        obj.bootloader = config_section.get('bootloader')
+        obj.flash_method = config_section.get('flash_method')
+        pass
+
 
 class KQFConfig(object):
-    config_flavors_path: PathLike
-    firmware_storage_path: PathLike
-    # Todo: also the string 'autodetect'
-    klipper_config_path: Optional[PathLike]
-    # Todo: also the string 'autodetect'
-    klipper_repo_path: PathLike
+    config_flavors_path: Optional[pathlib.Path]
+    firmware_storage_path: Optional[pathlib.Path]
+    _klipper_config: Optional[pathlib.Path]
+    _klipper_config_path: Optional[PathLike]
+    _klipper_config_auto: bool
+    _klipper_repo: Optional[pathlib.Path]
+    _klipper_repo_path: Optional[PathLike]
+    _klipper_repo_auto: bool
 
     mcus: List[KQFMCUConfig]
+
+    def __init__(self):
+        self.config_flavors_path = None
+        self.firmware_storage_path = None
+        self._klipper_config = None
+        self._klipper_config_path = None
+        self._klipper_config_auto = False
+        self._klipper_repo = None
+        self._klipper_repo_path = None
+        self._klipper_config_auto = False
 
     DEFAULT = textwrap.dedent("""\
         [KQF]
@@ -107,17 +151,13 @@ class KQFConfig(object):
         #   the special value 'autodetect' will try a few common locations, and should work with KIAUH.
         klipper_repo_path: autodetect
         
-        # klipper_config_path: The location of the klipper config to use when searching for mcus
+        # klipper_config_path: The location of the klipper config to use when searching for MCUs
         #   The special value 'autodetect' attempts to automatically find the klipper config
-        #   If this value is not present, mcus will not be autodetected"
+        #   If this value is not present, MCUs will not be autodetected"
         klipper_config_path: autodetect
         
         # config_flavors_path: Path to a directory of klipper .config files, relative to the location of the script
-        config_flavors_path: ./flavors
-        
-        # klipper_default_version: The default version of klipper to build
-        # if none is specified, no pull will be performed
-        #klipper_default_version:
+        config_flavors_path: ~/.kqf/flavors
         
         # firmware_storage: The path of where to store compiled firmware
         firmware_storage: ~/.kqf/firmware
@@ -132,6 +172,7 @@ class KQFConfig(object):
 
     @staticmethod
     def get(path="~/.kqf/kqf.cfg"):
+        logging.debug(f"Loading configuration from {path}")
         conf_path = pathlib.Path(path).expanduser()
         if not conf_path.exists():
             conf_path.parent.mkdir(parents=True, exist_ok=True)
@@ -142,10 +183,103 @@ class KQFConfig(object):
         kqf_conf_parser.read(conf_path)
         kqf_conf = KQFConfig()
         kqf_conf.__load_from_conf(kqf_conf_parser)
+        logging.debug(f"Loaded configuration from {path}")
         return kqf_conf
 
     def __load_from_conf(self, conf):
-        pass
+        if 'KQF' not in conf.sections():
+            raise ValueError("KQF section is missing from the configuration. (It's case sensitive)")
+
+        repo_path_str = conf.get('KQF', 'klipper_repo_path')
+        if not repo_path_str:
+            raise ValueError("Klipper repo path is not specified in configuration. It is required")
+        if repo_path_str == 'autodetect':
+            self._klipper_repo_path = None
+            self._klipper_repo_auto = True
+        elif pathlib.Path(repo_path_str).expanduser().is_dir():
+            self._klipper_repo_path = pathlib.Path(repo_path_str).expanduser()
+            self._klipper_repo_auto = False
+        else:
+            raise ValueError(f"Klipper repo path {repo_path_str} is invalid or does not exist")
+
+        config_path_str = conf.get('KQF', 'klipper_config_path')
+        if not config_path_str:
+            self._klipper_config_path = None
+            self._klipper_config_auto = False
+        elif config_path_str == 'autodetect':
+            self._klipper_config_path = None
+            self._klipper_config_auto = True
+        elif pathlib.Path(config_path_str).expanduser().is_file():
+            self._klipper_config_path = pathlib.Path(config_path_str).expanduser()
+            self._klipper_config_auto = False
+        else:
+            raise ValueError(f"Klipper repo path {repo_path_str} is invalid or does not exist")
+        mcus = [KQFMCUConfig.from_config(conf[conf_section]) for conf_section in conf.sections() if
+                conf_section == 'mcu' or conf_section.startswith('mcu ')]
+
+    @property
+    def klipper_config(self) -> Optional[pathlib.Path]:
+        if self._klipper_config:
+            return self._klipper_config
+        elif self._klipper_config_path:
+            self._klipper_config = pathlib.Path(self._klipper_config_path)
+            return self._klipper_config
+        elif self._klipper_config_auto:
+            config_path = self._find_klipper_config()
+            if config_path:
+                return config_path
+            else:
+                raise ValueError(
+                    "Could not autodetect klipper config location. Please set klipper_config_path in the KQF"
+                    "section of the KQF configuration")
+        else:
+            # Klipper config parsing is disabled
+            return None
+
+    __CONFIG_PATHS_TO_TRY = [
+        "~/printer_data/config/printer.cfg",
+        "/etc/klipper/printer.cfg"
+    ]
+
+    def _find_klipper_config(self) -> Optional[pathlib.Path]:
+        for path_to_try in self.__CONFIG_PATHS_TO_TRY:
+            path = pathlib.Path(path_to_try).expanduser()
+            if path.is_file():
+                return path
+        return None
+
+    @property
+    def klipper_repo(self) -> Optional[pathlib.Path]:
+        if self._klipper_repo:
+            return self._klipper_repo
+        elif self._klipper_repo_path:
+            self._klipper_repo = pathlib.Path(self._klipper_repo_path)
+            return self._klipper_repo
+        elif self._klipper_config_auto:
+            repo_path = self._find_klipper_repo()
+            if repo_path:
+                return repo_path
+            else:
+                raise ValueError(
+                    "Could not autodetect klipper config location. Please set klipper_config_path in the KQF"
+                    "section of the KQF configuration")
+        else:
+            # Klipper config parsing is disabled
+            return None
+
+    __REPO_PATHS_TO_TRY = [
+        "~/klipper",
+        "~/src/klipper",
+        "~/vcs/klipper"
+    ]
+
+    def _find_klipper_repo(self) -> pathlib.Path:
+        for path_to_try in self.__REPO_PATHS_TO_TRY:
+            path = pathlib.Path(path_to_try).expanduser()
+            if (path / ".git").is_dir() and (path / 'klippy' / 'klippy.py').is_file():
+                return path
+        raise ValueError(
+            "Could not autodetect klipper config location")
 
 
 if __name__ == '__main__':
