b18e75691bf67d765f762dba82badd86d490e2d3
Aaron Haun <aaron@haun.guru>
2024-01-30 01:50:48 -0500

    fix: Multiple mcus when flashing

--- kqf.py
+++ kqf.py
@@ -54,9 +54,8 @@ def entrypoint() -> None:
     build_flavor_spec.add_argument('--all', dest='build_all', action='store_true', help="Build all")
 
     flash_cmd = add_cmd(commands, 'flash', cmd_flash, help="Flash to a given MCU")
-    flash_flavor_spec = flash_cmd.add_mutually_exclusive_group(required=True)
-    flash_flavor_spec.add_argument('mcu', metavar='MCU', help="the mcu to flash", nargs='?'),
-    flash_flavor_spec.add_argument('--all', dest='flash_all', action='store_true', help="Build all")
+    flash_cmd.add_argument('--all', dest='flash_all', action='store_true', help="Build all")
+    flash_cmd.add_argument('mcu', metavar='MCU', help="the mcu to flash", nargs='*'),
     flash_cmd.add_argument(
                             "--build", dest='build_before_flash', action='store_true',
                             help="Build firmware for mcus before flashing")
@@ -110,11 +109,15 @@ def cmd_build(kqf: 'KQF', args):
 
 
 def cmd_flash(kqf: 'KQF', args):
+    if args.flash_all and len(args.mcu) > 0:
+        raise ValueError("Both '--all' and a list of flavors may not be specified")
     kqf.inventory()
     if args.flash_all:
         mcu_names_to_flash = kqf.list_mcus()
     else:
-        mcu_names_to_flash = [args.mcu]
+        mcu_names_to_flash = args.mcu
+    if not len(mcu_names_to_flash) > 0:
+        raise ValueError("No mcus are specified for flashing")
     mcus_to_flash = [kqf.get_mcu(mcu_name) for mcu_name in mcu_names_to_flash]
     if args.build_before_flash:
         flavors_to_build = set(mcu.flavor for mcu in mcus_to_flash)
