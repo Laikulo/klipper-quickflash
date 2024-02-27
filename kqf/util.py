5675dc4dcf3f2422a9ba9748d6b88962edd189c8
Aaron B. Haun <aaron@haun.guru>
2024-02-27 16:08:35 -0500

    feat: editor laucnher

--- kqf/util.py
+++ kqf/util.py
@@ -1,4 +1,7 @@
 import logging
+import os
+import pathlib
+import shutil
 import subprocess
 import json
 from typing import Optional
@@ -20,3 +23,25 @@ def get_can_interface_bitrate(ifname: str) -> Optional[str]:
         )
         return None
     return bitrate
+
+
+def launch_editor(filename: os.PathLike, editor: Optional[str] = None) -> None:
+    editor = editor or find_editor()
+    subprocess.run([
+        editor,
+        filename
+    ])
+
+
+EDITOR_ENV_TO_TRY = ['KQF_EDITOR', 'VISUAL', 'EDITOR']
+EDITORS_TO_TRY = ['sensible-editor', 'editor', 'vim', 'vi', 'emacs', 'nano', 'pico']
+
+
+def find_editor() -> str:
+    for i in EDITOR_ENV_TO_TRY:
+        if i in os.environ and shutil.which(os.environ[i]):
+            return os.environ[i]
+    for i in EDITORS_TO_TRY:
+        if shutil.which(i):
+            return i
+    raise RuntimeError("Unable to find an editor, please set the KQF_EDITOR, EDITOR, or VISUAL envvar")
